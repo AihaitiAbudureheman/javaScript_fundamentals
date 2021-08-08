b52fcd0c125bfad3aa8ed0741b2c514891b974f6
Aihaiti Abudureheman <baghrash1985@gmail.com>
2021-08-08 13:49:27 +0300

    New Changes

--- debugging_using_javaScript_console/demo.js
+++ debugging_using_javaScript_console/demo.js
@@ -1,4 +1,1 @@
-alert("Hi Alert!");
-console.log("Hi console!");
-alert("Is everything allright?");
-document.write("<h2>Hello World</h2>");
+
---
+++ debugging_using_javaScript_console/final/demo.js
@@ -0,0 +1,4 @@
+alert("Hi Alert!");
+console.log("Hi Console");
+alert("Is Everthing Allright?");
+document.write("<h2>Hello World</h2>");
